a197c564c8fcb4e0da4692c3f45875bbc154a806
Vitaliy <theyamshikov@yandex.ru>
2020-09-08 20:55:21 +0600

    upd

---
+++ bin/package/push packages.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znbundle/article && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znbundle/messenger && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znbundle/notify && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znbundle/queue && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znbundle/reference && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znbundle/storage && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znbundle/user && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zncore/base && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zncore/db && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zncore/ddd && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zncrypt/base && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zncrypt/jwt && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zncrypt/pki && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zncrypt/tunnel && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zndoc/linux && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zndoc/rest-api && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zndoc/team && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znexample/composer-package && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znexample/database && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znexample/phpunit && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znfixture/geo && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znfixture/info-bot && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znfork/danog_madelineproto && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znlib/rest && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znlib/socket && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znsandbox/bundle && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znsandbox/sandbox && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znsandbox/telegram && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znsandbox/web && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/znsandbox/yii2-legacy && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zntool/dev && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zntool/test && git add . && git commit -m upd && git push
+
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zntpl/backend.vkrg && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zntpl/symfony4 && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zntpl/telegram-bot && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zntpl/telegram-client && git add . && git commit -m upd && git push
+cd /home/vitaliy/common/var/www/tpl/all/vendor/zntpl/yii2 && git add . && git commit -m upd && git push
